84355b8fe6e69ed4bcb06337216b3db1bb169f61
adrian <asanz641gmail.com>
2022-01-09 13:13:20 +0100

    changes

--- src/database.js
+++ src/database.js
@@ -1,14 +1,10 @@
 import mongoose from 'mongoose'
-/*
-mongoose.connect('mongodb://localhost:27017/mydb')
-.then(db => console.log('Db is connected'))
-.catch(error => console.log(error))*/
+
 mongoose.connect('mongodb://localhost:27017/companydb',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    //useCreateIndex : true
-    //useFindAndModify: true
+    
   }
 ).then(db => console.log('Db is connected'))
 .catch(error => console.log(error));
